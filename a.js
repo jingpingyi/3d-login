3c2c71c44e72a28254e0343bce05067d659db965
YJP <yijingping2012@yeah.net>
2018-10-11 17:05:19 +0800

    waitingg---

--- a.js
+++ a.js
@@ -1,1 +1,16 @@
-a.js
+function $(selector) {
+	return document.querySelector(selector);
+}
+function $$(selector) {
+	return document.querySelectorAll(selector);
+}
+$('.flip-modal').addEventListener('click', function(e){
+	if(e.target.classList.contains('login')){
+		$('.flip-modal').classList.remove('register')
+		$('.flip-modal').classList.add('login')
+	}
+	if(e.target.classList.contains('register')){
+		$('.flip-modal').classList.add('register')
+		$('.flip-modal').classList.remove('login')
+	}
+})
